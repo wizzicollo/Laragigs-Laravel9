1fb38fe34d88388b113a82435232b87481c9604f
collins kiprutoh <kiprutohcollo@gmail.com>
2022-06-10 00:13:00 +0300

    started the project

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum hfejwiue uhfioowi 
+                 huuhoiwowoiewi uhowehfioewho ouhofhwehe 
+                 uifoewuhewewu hfejwiue uhfioowi 
+                 huuhoiwowoiewi uhowehfioewho ouhofhwehe 
+                 uifoewuhewewu' 
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum hfejwiue uhfioowi 
+                 huuhoiwowoiewi uhowehfioewho ouhofhwehe 
+                 uifoewuhewewu hfejwiue uhfioowi 
+                 huuhoiwowoiewi uhowehfioewho ouhofhwehe 
+                 uifoewuhewewu' 
+            ]
+        ]
+    ]);
 });
